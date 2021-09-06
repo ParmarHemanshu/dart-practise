2510676de0383a581c9f36c8af8d5758ac38788d
hemanshu733 <parmarhemanshu733@gmail.com>
2021-09-06 17:30:26 +0530

    SumChecker : Find possible sum combinations of given number from the list.

---
+++ Basics/SumChecker.dart
@@ -0,0 +1,27 @@
+class SumChecker{
+  
+  //check for elements whoes addition is given num
+  void getPairsOfNumber(List<int> list,int num){
+    for (int i=0;i<list.length;i++){
+      for(int j=i+1;j<list.length;j++){
+        if(list[i]+list[j]==num){
+         
+          //print non repeated numbers
+         print(list[i].toString()+","+list[j].toString());
+        
+        }
+      }
+    }
+  }
+}
+
+
+void main(){
+  
+  List<int> list=[2,3,5,6,4,1];
+  
+  SumChecker s1=new SumChecker();
+  
+  //pass list and number whose sum elements u want to find
+  s1.getPairsOfNumber(list,7);
+}
